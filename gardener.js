084fa87a9c84003d11b18a4621aba3b35415a6ff
developer119 <developer119korea@gmail.com>
2018-05-30 22:51:40 +0900

    주중에 공휴일이 포함된 경우 메세지를 보내지 않는다

--- gardener.js
+++ gardener.js
@@ -44,11 +44,46 @@ var loadConfig = function () {
         var data = fs.readFileSync(__dirname + "/data/config.json", 'utf8');
         var resource = {};
         resource = JSON.parse(data);
-        return resource["webhook_url"];
+        return resource;
     }
     catch (err) {
         console.log(err);
     }
+}
+
+var checkEventDay = function (Host, ProjectKey, baseUrl, callbackAction) {
+    const config = {
+        host: Host,
+        Referer: "",
+        headers: {
+            "TDCProjectKey": ProjectKey,
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    };
+
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1; //January is 0!
+    var day = today.getDay();
+    var queryUrl = baseUrl + "?type=h&year=" + year + "&month=" + month + "&day=" + day;
+    var url = queryUrl;
+
+    request.get(url, config, function (err, response, body) {
+        console.log("event Day Check Url : " + url);
+        if (err) {
+            console.error('err: ', err);
+            return;
+        }
+        console.log('responese : ', body);
+        if (parseInt(body["totalResult"]) > 0) {
+            console.log("오늘은 공휴일입니다.");
+        }
+        else {
+            console.log("오늘은 업무일입니다.");
+            callbackAction();
+        }
+    });
 }
 
 var rule = new schedule.RecurrenceRule();
@@ -56,6 +91,13 @@ rule.dayOfWeek = [0, new schedule.Range(1, 5)];
 rule.hour = 10;
 rule.minute = 0;
 
-var job = schedule.scheduleJob(rule,  function() { 
-    SendMessage(loadConfig())
+var job = schedule.scheduleJob(rule, function () {
+    BreakFast();
 });
+
+var BreakFast = function () {
+    var config = loadConfig();
+    checkEventDay(config["EventDay-host"], config["EventDay-TDCProjecyKey"], config["EventDay-URL"], function () {
+        SendMessage(config["Webhook_Url"]);
+    });
+};
