65a0307f9340918e9600705fb13254b8329486bf
developer119 <developer119korea@gmail.com>
2018-06-07 21:29:08 +0900

    설정값을 불러오는 코드 분리

--- gardener.js
+++ gardener.js
@@ -1,26 +1,14 @@
 var express = require('express');
 var request = require('request');
-var fs = require('fs');
-var app = express();
 var schedule = require('node-schedule');
 var jandi = require('./jandi.js');
 var eventday = require('./eventday.js');
+var config = require('./config.js');
+var app = express();
 
 var server = app.listen(3030, function () {
     console.log("Express server has started on port 3030");
 })
-
-var loadConfig = function () {
-    try {
-        var data = fs.readFileSync(__dirname + "/data/config.json", 'utf8');
-        var resource = {};
-        resource = JSON.parse(data);
-        return resource;
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
 
 var rule = new schedule.RecurrenceRule();
 // rule.dayOfWeek = [0, new schedule.Range(1, 5)];
@@ -33,8 +21,8 @@ var job = schedule.scheduleJob(rule, function () {
 });
 
 var BreakFast = function () {
-    var config = loadConfig();
-    eventday.checkEventDay(config["EventDay-host"], config["EventDay-TDCProjecyKey"], config["EventDay-URL"], function () {
-        jandi.sendMessage(config["Webhook_Url"]);
+    var configValue = config.loadConfig();
+    eventday.checkEventDay(configValue["EventDay-host"], configValue["EventDay-TDCProjecyKey"], configValue["EventDay-URL"], function () {
+        jandi.sendMessage(configValue["Webhook_Url"]);
     });
 };
